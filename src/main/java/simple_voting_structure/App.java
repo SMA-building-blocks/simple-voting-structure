a7cff4d66d95b63b4bee1803dc1a00feae4ebb0c
gabrielm2q <mariano.gabriel2q@gmail.com>
2025-04-12 10:30:23 -0300

    :recycle: refactoring app manager
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>

--- src/main/java/simple_voting_structure/App.java
+++ src/main/java/simple_voting_structure/App.java
@@ -6,12 +6,6 @@ package simple_voting_structure;
 import jade.core.AID;
 import jade.wrapper.AgentController;
 import jade.wrapper.AgentContainer;
-import jade.wrapper.ContainerController;
-import jade.core.ProfileImpl;
-import jade.core.Runtime;
-import jade.domain.DFService;
-import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.lang.acl.ACLMessage;
 
 import java.io.IOException;
@@ -19,43 +13,32 @@ import java.io.IOException;
 /**
  * Class that set the main agent and it's actions
  */
-public class App extends BaseAgent{
+public class App extends BaseAgent {
 
 	private static final long serialVersionUID = 1L;
 	
-	private static final String START = "START";
-	
-	private AgentController m1 = null;
-	
-    @Override
+	@Override
 	protected void setup() {
 		
 		System.out.println("Ola Mundo! ");
 		System.out.println("Meu nome: " + getLocalName());
 		
-		try {
-			DFAgentDescription dfd = new DFAgentDescription();
-			dfd.setName(getAID());
-			DFService.register(this, dfd);
-			System.out.println(getLocalName()+" REGISTERED WITH THE DF");
-		} catch (FIPAException e) {
-			e.printStackTrace();
-		}
+		registerDF(this, "Manager", "manager");
 		
 		System.out.println("Starting Agents...");
 		
-		String m1AgentName = "mediator";
-		
-		try {
-			AgentContainer container = (AgentContainer)getContainerController(); // get a container controller for creating new agents
-			m1 = container.createNewAgent(m1AgentName, "simple_voting_structure.Mediator", null);
-			m1.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		String m1AgentName = "Mediator";
+		launchAgent(m1AgentName, "simple_voting_structure.Mediator", null);
 		System.out.println("Agents started...");
 		
+		pauseSystem();
+		
+		// send them a message demanding start;
+		System.out.println("Starting system!");		
+		sendMessage(m1AgentName, ACLMessage.INFORM, MessageTypes.START.name());
+	}
+
+	private void pauseSystem() {
 		try {
             System.out.println("The system is paused -- this action is only here to let you activate the sniffer on the agents, if you want (see documentation)");
             System.out.println("Press enter in the console to start the agents");
@@ -63,15 +46,15 @@ public class App extends BaseAgent{
         } catch (IOException e) {
             e.printStackTrace();
         }
-		
-		System.out.println("Starting system!");
-		
-		// send them a message demanding start;
-		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-		msg.setContent(START);
+	}
 
-		msg.addReceiver(new AID(m1AgentName, AID.ISLOCALNAME));
-		
-		send(msg);
+	private void launchAgent(String agentName, String className, Object[] args) {
+		try {
+			AgentContainer container = (AgentContainer)getContainerController(); // get a container controller for creating new agents
+			AgentController newAgent = container.createNewAgent(agentName, className, args);
+			newAgent.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/simple_voting_structure/BaseAgent.java
+++ src/main/java/simple_voting_structure/BaseAgent.java
@@ -1,12 +1,18 @@
 package simple_voting_structure;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 // import jade.wrapper.StaleProxyException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
 public abstract class BaseAgent extends Agent {
 	
+	private static final long serialVersionUID = 1L;
+
 	public enum MessageTypes {
 		ODD("ODD"), EVEN("EVEN"), REQUEST("REQUEST"), ANSWER("ANSWER"), THANKS("THANKS"), START("START");
 		
@@ -21,7 +27,33 @@ public abstract class BaseAgent extends Agent {
 		}
 	};
 	
-	protected void setup () {}
+	@Override
+	protected void setup() {}
+	
+	protected void registerDF(Agent regAgent, String sdName, String sdType) {
+		try {
+			DFAgentDescription dfd = new DFAgentDescription();
+			dfd.setName(getAID());
+			
+			ServiceDescription sd = new ServiceDescription();
+			sd.setType(sdType);
+			sd.setName(sdName);
+			
+			dfd.addServices(sd);
+			
+			DFService.register(regAgent, dfd);
+			System.out.println(getLocalName()+" REGISTERED WITH THE DF");
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void sendMessage(String agentName, int performative, String content) {
+		ACLMessage msg = new ACLMessage(performative);
+		msg.setContent(content);
+		msg.addReceiver(new AID(agentName, AID.ISLOCALNAME));
+		send(msg);
+	}
 	
 	protected void takeDown() {
 		// Deregister with the DF
