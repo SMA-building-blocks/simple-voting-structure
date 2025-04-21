85fbd7df183c5b29d4e91b7a8bb7f0c9eb1f034e
gabrielm2q <mariano.gabriel2q@gmail.com>
2025-04-21 20:55:09 -0300

    :fire: removing unused variables
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>

--- src/main/java/simple_voting_structure/App.java
+++ src/main/java/simple_voting_structure/App.java
@@ -3,14 +3,13 @@
  */
 package simple_voting_structure;
 
-import jade.wrapper.AgentController;
-import jade.wrapper.AgentContainer;
-import jade.lang.acl.ACLMessage;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 
+import jade.lang.acl.ACLMessage;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
 
 /**
  * Class that set the main agent and it's actions
@@ -18,68 +17,74 @@ import java.util.logging.Level;
 public class App extends BaseAgent {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void setup() {
-		
+
 		loggerSetup();
-		
-		registerDF(this, "Manager", "manager");
-		
+
+		registerDF(this, "Creator", "creator");
+
 		logger.log(Level.INFO, "Starting Agents...");
-		
+
 		logger.log(Level.INFO, "Creating voters...");
 
 		ArrayList<String> votersName = new ArrayList<>();
-		
+
 		Object[] args = getArguments();
 		int votersQuorum = 0;
 		if (args != null && args.length > 0) {
-			votersQuorum =  Integer.parseInt(args[0].toString());
+			votersQuorum = Integer.parseInt(args[0].toString());
 		}
-		
+
 		int votingStarter = rand.nextInt(votersQuorum);
-		
+
 		logger.log(Level.INFO, String.format("Agent number %d will request to the mediator!", votingStarter));
-				
-		for ( int i = 0; i < votersQuorum; ++i ) votersName.add("voter_" + i);
+
+		for (int i = 0; i < votersQuorum; ++i)
+			votersName.add("voter_" + i);
 
 		try {
 			// create agents on the same container of the creator agent
 			AgentContainer container = getContainerController(); // get a container controller for creating
-			
+
 			votersName.forEach(voter -> {
-				this.launchAgent(voter, "simple_voting_structure.Voter", null);	
-				logger.log(Level.INFO, String.format("%s CREATED AND STARTED NEW VOTER: %s ON CONTAINER %s", getLocalName(), voter, container.getName()));
+				this.launchAgent(voter, "simple_voting_structure.Voter", null);
+				logger.log(Level.INFO,
+						String.format("%s CREATED AND STARTED NEW VOTER: %s ON CONTAINER %s", getLocalName(),
+								voter, container.getName()));
 			});
 		} catch (Exception any) {
-			logger.log(Level.SEVERE, String.format("%s ERROR WHILE CREATING AGENTS %s", ANSI_RED , ANSI_RESET));
+			logger.log(Level.SEVERE, String.format("%s ERROR WHILE CREATING AGENTS %s", ANSI_RED, ANSI_RESET));
 			any.printStackTrace();
 		}
-		
+
 		String m1AgentName = "Mediator";
-		launchAgent(m1AgentName, "simple_voting_structure.Mediator", votersName.toArray());
-		
+		launchAgent(m1AgentName, "simple_voting_structure.Mediator", null);
+
 		logger.log(Level.INFO, "Agents started...");
 		pauseSystem();
-		
+
 		// send them a message demanding start
 		logger.log(Level.INFO, "Starting system!");
 
 		sendMessage(votersName.get(votingStarter), ACLMessage.INFORM, START);
-		logger.log(Level.INFO,String.format("%s SENT START MESSAGE  TO %s", getLocalName(), votersName.get(votingStarter)));
+		logger.log(Level.INFO,
+				String.format("%s SENT START MESSAGE  TO %s", getLocalName(), votersName.get(votingStarter)));
 	}
-	
 
 	private void pauseSystem() {
 		try {
-			logger.log(Level.WARNING, String.format("%s The system is paused -- this action is here only to let you activate the sniffer on the agents, if you want (see documentation) %s", ANSI_YELLOW, ANSI_RESET));
-			logger.log(Level.WARNING, String.format("%s Press enter in the console to start the agents %s", ANSI_YELLOW , ANSI_RESET));
-            System.in.read();
-        } catch (IOException e) {
-        	logger.log(Level.SEVERE, String.format("%s ERROR STARTING THE SYSTEM %s",ANSI_RED, ANSI_RESET));
-            e.printStackTrace();
-        }
+			logger.log(Level.WARNING, String.format(
+					"%s The system is paused -- this action is here only to let you activate the sniffer on the agents, if you want (see documentation) %s",
+					ANSI_YELLOW, ANSI_RESET));
+			logger.log(Level.WARNING,
+					String.format("%s Press enter in the console to start the agents %s", ANSI_YELLOW, ANSI_RESET));
+			System.in.read();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, String.format("%s ERROR STARTING THE SYSTEM %s", ANSI_RED, ANSI_RESET));
+			e.printStackTrace();
+		}
 	}
 
 	private void launchAgent(String agentName, String className, Object[] args) {
@@ -88,7 +93,7 @@ public class App extends BaseAgent {
 			AgentController newAgent = container.createNewAgent(agentName, className, args);
 			newAgent.start();
 		} catch (Exception e) {
-			logger.log(Level.SEVERE, String.format("%s ERROR WHILE LAUNCHING AGENTS %s", ANSI_RED , ANSI_RESET));
+			logger.log(Level.SEVERE, String.format("%s ERROR WHILE LAUNCHING AGENTS %s", ANSI_RED, ANSI_RESET));
 			e.printStackTrace();
 		}
 	}
--- src/main/java/simple_voting_structure/BaseAgent.java
+++ src/main/java/simple_voting_structure/BaseAgent.java
@@ -1,5 +1,10 @@
 package simple_voting_structure;
 
+import java.util.Random;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
@@ -11,20 +16,11 @@ import jade.domain.FIPAAgentManagement.SearchConstraints;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 
-import java.util.logging.Logger;
-
-import java.util.Random;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-
 public abstract class BaseAgent extends Agent {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	public static final String ODD = "ODD";
-	public static final String EVEN = "EVEN";
+
 	public static final String REQUEST = "REQUEST";
-	public static final String ANSWER = "ANSWER";
 	public static final String THANKS = "THANKS";
 	public static final String START = "START";
 	public static final String VOTEID = "VOTEID";
@@ -45,157 +41,158 @@ public abstract class BaseAgent extends Agent {
 	public static final String ANSI_PURPLE = "\u001B[35m";
 	public static final String ANSI_CYAN = "\u001B[36m";
 	public static final String ANSI_WHITE = "\u001B[37m";
-	
+
 	protected static final Random rand = new Random();
 
 	protected int votingCode;
-	
+
 	protected static final Logger logger = Logger.getLogger(BaseAgent.class.getName());
-	
+
 	@Override
-	protected void setup() {}
-	
-	protected CyclicBehaviour handleMessages () {
+	protected void setup() {
+	}
+
+	protected CyclicBehaviour handleMessages() {
 		return new CyclicBehaviour(this) {
 			private static final long serialVersionUID = 1L;
 
 			public void action() {
 				ACLMessage msg = receive();
-				
-				if ( msg == null ) block();
+
+				if (msg == null)
+					block();
 				else {
-					switch ( msg.getPerformative() ) {
-					case ACLMessage.INFORM:
-						addBehaviour(handleInform(msg));
-						break;
-					case ACLMessage.REQUEST:
-						addBehaviour(handleRequest(msg));
-						break;
-					default:
-						logger.log(Level.INFO, 
-								String.format("%s RECEIVED UNEXPECTED MESSAGE PERFORMATIVE FROM %s", getLocalName(), msg.getSender().getLocalName()));
+					switch (msg.getPerformative()) {
+						case ACLMessage.INFORM:
+							addBehaviour(handleInform(msg));
+							break;
+						case ACLMessage.REQUEST:
+							addBehaviour(handleRequest(msg));
+							break;
+						default:
+							logger.log(Level.INFO,
+									String.format("%s RECEIVED UNEXPECTED MESSAGE PERFORMATIVE FROM %s", getLocalName(),
+											msg.getSender().getLocalName()));
 					}
 				}
 			}
 		};
 	}
-	
-	protected OneShotBehaviour handleInform ( ACLMessage msg ) {
+
+	protected OneShotBehaviour handleInform(ACLMessage msg) {
 		return new OneShotBehaviour(this) {
 			private static final long serialVersionUID = 1L;
 
-			public void action () {
+			public void action() {
 				/*
 				 * TO-DO:
 				 * IMPLEMENT THIS METHOD BEHAVIOUR ON CONCRETE CLASS
-				 * */
+				 */
 				msg.createReply();
 			}
 		};
 	}
-	
-	protected OneShotBehaviour handleRequest (ACLMessage msg) {
+
+	protected OneShotBehaviour handleRequest(ACLMessage msg) {
 		return new OneShotBehaviour(this) {
 			private static final long serialVersionUID = 1L;
 
-			public void action () {
+			public void action() {
 				/*
 				 * TO-DO:
 				 * IMPLEMENT THIS METHOD BEHAVIOUR ON CONCRETE CLASS
-				 * */
+				 */
 				msg.createReply();
 			}
 		};
 	}
-	
+
 	protected void registerDF(Agent regAgent, String sdName, String sdType) {
 		try {
 			DFAgentDescription dfd = new DFAgentDescription();
 			dfd.setName(getAID());
-			
-			
+
 			ServiceDescription sd = new ServiceDescription();
 			sd.setType(sdType);
 			sd.setName(sdName);
-			
-			
-			DFAgentDescription [] found = DFService.search(this, dfd);
-			
+
+			DFAgentDescription[] found = DFService.search(this, dfd);
+
 			dfd.addServices(sd);
-			
-			if ( found.length == 0 ) {
+
+			if (found.length == 0) {
 				DFService.register(regAgent, dfd);
 			} else {
 				found[0].addServices(sd);
 				DFService.modify(regAgent, found[0]);
 			}
-				
-			logger.log(Level.INFO, String.format("%s REGISTERED WITH THE DF", getLocalName()) );
+
+			logger.log(Level.INFO, String.format("%s REGISTERED WITH THE DF", getLocalName()));
 		} catch (FIPAException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	protected void sendMessage(String agentName, int performative, String content) {
 		ACLMessage msg = new ACLMessage(performative);
 		msg.setContent(content);
 		msg.addReceiver(new AID(agentName, AID.ISLOCALNAME));
 		send(msg);
 	}
-	
-	protected DFAgentDescription[] searchAgentByType (String type) {
+
+	protected DFAgentDescription[] searchAgentByType(String type) {
 		SearchConstraints sc = new SearchConstraints();
 		sc.setMaxResults(-1L);
 		DFAgentDescription search = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();
-		DFAgentDescription [] foundAgents = null;
-		
+		DFAgentDescription[] foundAgents = null;
+
 		sd.setType(type);
 		search.addServices(sd);
-		
+
 		try {
 			foundAgents = DFService.search(this, search, sc);
-		} catch ( Exception any ) {
+		} catch (Exception any) {
 			any.printStackTrace();
 		}
-		
+
 		return foundAgents;
 	}
-	
-	protected DFAgentDescription[] searchAgentByType (String [] type) {
+
+	protected DFAgentDescription[] searchAgentByType(String[] type) {
 		SearchConstraints sc = new SearchConstraints();
 		sc.setMaxResults(-1L);
 		DFAgentDescription search = new DFAgentDescription();
-		
-		DFAgentDescription [] foundAgents = null;
-		
-		for ( int i = 0; i < type.length; ++i ) {
+
+		DFAgentDescription[] foundAgents = null;
+
+		for (int i = 0; i < type.length; ++i) {
 			ServiceDescription sd = new ServiceDescription();
 			sd.setType(type[i]);
 			search.addServices(sd);
 		}
-		
+
 		try {
 			foundAgents = DFService.search(this, search, sc);
-		} catch ( Exception any ) {
+		} catch (Exception any) {
 			any.printStackTrace();
 		}
-		
+
 		return foundAgents;
 	}
-	
+
 	@Override
 	protected void takeDown() {
 		// Deregister with the DF
 		try {
 			DFService.deregister(this);
-			logger.log(Level.INFO, String.format("%s DEREGISTERED WITH THE DF",getLocalName()));
+			logger.log(Level.INFO, String.format("%s DEREGISTERED WITH THE DF", getLocalName()));
 
 		} catch (FIPAException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	protected void loggerSetup() {
 		ConsoleHandler handler = new ConsoleHandler();
 		handler.setFormatter(new LogFormatter());
